c8a504a58ab56ee68d8f6df65e198f98ef2f85f2
Emil Andersson <e.andersson22@gmail.com>
2021-01-08 00:55:55 +0100

    visuals for cards complete

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -8,11 +8,9 @@ var symbol = document.getElementById("symbol");
 
 $("#songs_btn").hide();
 $("#playlist_btn").hide();
-$("#loading").hide();
+$(".loading").hide();
+$(".more_songs").hide();
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
 /*
 TODO:
 
@@ -36,7 +34,7 @@ function getCurrentLocation() {
     navigator.geolocation.getCurrentPosition(locationFromCoords);
     //When the button is clicked it it no longer shows
     $("#map").fadeOut();
-    $("#genreButtons").delay(500).fadeIn(800);
+    $("#genreButtons").delay(100).fadeIn(800);
   } else {
     coord.innerHTML = "Geolocation is not supported by this browser.";
   }
@@ -44,12 +42,11 @@ function getCurrentLocation() {
 
 
 function locationFromCoords(position) {
-  console.log(typeof position)
-  console.log(position)
   $.ajax({
     method: "GET",
     url: "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude + "&key=AIzaSyALNSg4-mot96aQpauSbHWln_tZMCvx5fw"
   }).done(function (response) {
+    
     var location = response.results[8].formatted_address
     fetchCoords(position, location)
   })
@@ -62,12 +59,7 @@ function locationFromCoordsMap(position) {
     method: "GET",
     url: "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ parsed.lat + "," + parsed.lng + "&key=AIzaSyALNSg4-mot96aQpauSbHWln_tZMCvx5fw"
   }).done(function (response) {
-    console.log(response.status)
-    console.log(response)
-    console.log(response.results.length)
     var location = ""
-
-
     if (response.results.length >= 9){
       location = response.results[8].formatted_address;
     }else if(response.results.length >= 5){
@@ -83,35 +75,46 @@ function locationFromCoordsMap(position) {
 }
 
 function fetchTracks(genre) {
-  $("#genreButtons").fadeOut();
-  $("#loading").delay(500).fadeIn();
+  $("#genreButtons").hide();
+  $(".loading").fadeIn();
+  window.scrollTo(0,document.body.scrollHeight);
   //fetches the current weather from the current_weather paragraph
   var weather = document.getElementById("current_weather").innerHTML;
+  
   $.ajax({
     method: "GET",
     url: "http://localhost:8888/api/tracks/" + weather + "/" + genre
   }).done(function (response) {
     parsed = JSON.parse(response);
-    displayTracks(parsed, genre);
+    displayTracks(parsed);
+    
   })
 }
 
 //generate card for song
-function displayTracks(songs, genre) {
-  console.log(genre)
-  $("#loading").delay(3350).fadeOut(800);
+function displayTracks(songs) {
+  var randomClassName = Math.floor(Math.random() * 9999999);
+  $(".loading").delay(3350).fadeOut(800);
+
+
+    /* 
+      Generate a card for every track that has been fetched
+    */
   for (var songs = 0; songs < parsed.tracks.length; songs++) {
     var artists = []
+
+    /* 
+      For every artist that a song has, push that artist to an array.
+    */
     for (var i = 0; i < parsed.tracks[songs].artists.length; i++){
       artists.push(parsed.tracks[songs].artists[i].name)
     }
-   
-    var card = document.createElement('div');
-    card.className = "card text-white bg-dark p-3 m-3 card_songs";
-    card.id = "song_card";
-    card.style = "width: 12rem;";
-
     
+    var card = document.createElement('div');
+    card.className = "card text-white bg-dark p-3 m-3 " + randomClassName;
+    card.id = "song_card";
+    card.style = "width: 14rem;";
+
     card.innerHTML = '<img class="card-img-top" src="https://i.scdn.co/image/107819f5dc557d5d0a4b216781c6ec1b2f3c5ab2"  alt="Card image cap">';
     card.innerHTML += '<div class="card-body text-center">';
     card.innerHTML += '<h5 class="card-title">' + parsed.tracks[songs].name + '</h5>';
@@ -119,11 +122,13 @@ function displayTracks(songs, genre) {
     card.innerHTML += '<a href="https://open.spotify.com/track/' + parsed.tracks[songs].id + '" target="_blank" class="btn btn-secondary btn-block">Lyssna!</a>';
     card.innerHTML += '</div>';
     card.innerHTML += '</div>';
+
     document.getElementById("songs").appendChild(card);
-    $(".card_songs").hide();
-    
+    $("." + randomClassName).hide();
   }
-  $(".card_songs").delay(4700).fadeIn(800);
+
+  $("." + randomClassName).delay(4100).fadeIn(800);
+  $(".more_songs").delay(4500).fadeIn(800);
 } 
 
 
@@ -131,7 +136,6 @@ function displayTracks(songs, genre) {
   API function that fetches position depending on where the user is located and feeds it to our API
 */
 function fetchCoords(position, location) {
-  console.log(location)
   $.ajax({
     method: "GET",
     url: "http://localhost:8888/api/weather/"  + position.coords.latitude + "/" + position.coords.longitude
@@ -174,12 +178,14 @@ function showDataMap(parsed, APIresponse, location) {
   temp.innerHTML = parsed_response.air_temperature.toFixed(0) + "°C";
 
   var symbolWeather = '<img src="/img/'+parsed_response.symbol_code+'.svg"' + 'width="200" height="200" alt="weatherSymbol">';
+
   symbol.innerHTML = symbolWeather;
 
 }
 
 /*
 Displays return from location call
+
 */
 function showData(position, APIresponse, location) {
   parsed_response = JSON.parse(APIresponse)
